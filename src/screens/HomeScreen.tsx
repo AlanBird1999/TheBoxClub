575c5717a9dac51cf6610f9ef4c6db1e7f256c1e
alanbird1999 <alanbird1999@gmail.com>
2022-04-23 15:32:50 -0600

    get users from backend

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -5,11 +5,17 @@ import {
   SafeAreaView,
   FlatList,
   TouchableOpacity,
+  Alert,
 } from "react-native";
 import Amplify, { Auth } from "aws-amplify";
 import AWSConfig from "../aws-exports";
 
 import Room from "../components/Room";
+import * as queries from "../graphql/queries";
+import * as mutations from "../graphql/mutations";
+import * as subscriptions from "../graphql/subscriptions";
+import { useState } from "react";
+import { API } from "aws-amplify";
 
 Amplify.configure(AWSConfig);
 
@@ -18,6 +24,47 @@ interface homeProps {
 }
 
 export default function HomeScreen(props: homeProps) {
+  const [data, setData] = useState(null);
+  Auth.currentUserInfo()
+    .then(async (userInfo) => {
+      console.log("user info in home screen", userInfo);
+      const dataUser: any = await API.graphql({
+        query: queries.getUser,
+        variables: { id: userInfo.id },
+      });
+      console.log(dataUser);
+      if (!dataUser.data.getUser) {
+        const residenceDetails = {
+          rName: userInfo.username,
+        };
+        const newResidence = await API.graphql({
+          query: mutations.createResidence,
+          variables: { input: residenceDetails },
+        });
+        const userDeatails = {
+          id: userInfo.id,
+          UserName: userInfo.username,
+        };
+
+        const newUser = await API.graphql({
+          query: mutations.createUser,
+          variables: { input: userDeatails },
+        });
+        console.log("newUser", newUser);
+        //TODO: right around here if we want the ability to have multiple users to a single residence
+        //this is where we can prompt them to give a residence head
+      }
+    })
+    .catch((error) => {
+      console.log("error", error);
+      Alert.alert("Error getting user details", error.message, [
+        {
+          text: "OK",
+          style: "cancel",
+        },
+      ]);
+    });
+
   return (
     <SafeAreaView style={styles.container}>
       <FlatList
@@ -34,7 +81,7 @@ export default function HomeScreen(props: homeProps) {
       <View style={styles.buttonView}>
         <TouchableOpacity
           style={styles.button}
-          onPress={() => addItem(props.navigation)}
+          onPress={() => addItem(props.navigation, data)}
         >
           <Text style={styles.icon}>+</Text>
         </TouchableOpacity>
@@ -43,7 +90,7 @@ export default function HomeScreen(props: homeProps) {
   );
 }
 
-function addItem(navigation: any) {
+function addItem(navigation: any, data: any) {
   navigation.navigate("AddItemNavigation", data);
 }
 
@@ -52,7 +99,7 @@ const styles = StyleSheet.create({
     padding: 20,
     paddingBottom: 0,
     flex: 1,
-    alignItems: 'center',
+    alignItems: "center",
   },
   input: {
     height: 50,
@@ -79,160 +126,160 @@ const styles = StyleSheet.create({
   },
 });
 
-const data = [
-  {
-    name: "Basement",
-    containers: [
-      {
-        name: "Box 1",
-        key: 1,
-        items: [
-          { name: "item 1", description: "item 1 description", key: 1 },
-          { name: "item 2", description: "item 2 description", key: 2 },
-        ],
-      },
-      {
-        name: "Box 2",
-        key: 2,
-        items: [
-          { name: "item 1", description: "item 1 description", key: 1 },
-          { name: "item 2", description: "item 2 description", key: 2 },
-        ],
-      },
-    ],
-    key: 1,
-  },
-  {
-    name: "Storage Room",
-    containers: [
-      {
-        name: "Box 1",
-        key: 1,
-        items: [
-          { name: "item 1", description: "item 1 description", key: 1 },
-          { name: "item 2", description: "item 2 description", key: 2 },
-        ],
-      },
-      {
-        name: "Box 2",
-        key: 2,
-        items: [
-          { name: "item 1", description: "item 1 description", key: 1 },
-          { name: "item 2", description: "item 2 description", key: 2 },
-        ],
-      },
-      {
-        name: "Box 3",
-        key: 3,
-        items: [
-          { name: "item 1", description: "item 1 description", key: 1 },
-          { name: "item 2", description: "item 2 description", key: 2 },
-        ],
-      },
-      {
-        name: "Box 4",
-        key: 4,
-        items: [
-          { name: "item 1", description: "item 1 description", key: 1 },
-          { name: "item 2", description: "item 2 description", key: 2 },
-        ],
-      },
-    ],
-    key: 2,
-  },
-  {
-    name: "Attic",
-    containers: [
-      {
-        name: "Christmas Box",
-        key: 1,
-        items: [
-          { name: "item 1", description: "item 1 description", key: 1 },
-          { name: "item 2", description: "item 2 description", key: 2 },
-        ],
-      },
-      {
-        name: "Haloween",
-        key: 2,
-        items: [
-          { name: "item 1", description: "item 1 description", key: 1 },
-          { name: "item 2", description: "item 2 description", key: 2 },
-        ],
-      },
-    ],
-    key: 3,
-  },
-  {
-    name: "Garage",
-    containers: [
-      {
-        name: "Tool Box",
-        key: 1,
-        items: [
-          { name: "item 1", description: "item 1 description", key: 1 },
-          { name: "item 2", description: "item 2 description", key: 2 },
-        ],
-      },
-      {
-        name: "Car parts",
-        key: 2,
-        items: [
-          { name: "item 1", description: "item 1 description", key: 1 },
-          { name: "item 2", description: "item 2 description", key: 2 },
-        ],
-      },
-    ],
-    key: 4,
-  },
-  {
-    name: "Spare Room",
-    containers: [
-      {
-        name: "Decorations",
-        key: 1,
-        items: [
-          { name: "item 1", description: "item 1 description", key: 1 },
-          { name: "item 2", description: "item 2 description", key: 2 },
-        ],
-      },
-      {
-        name: "Blankets",
-        key: 2,
-        items: [
-          { name: "item 1", description: "item 1 description", key: 1 },
-          { name: "item 2", description: "item 2 description", key: 2 },
-        ],
-      },
-    ],
-    key: 5,
-  },
-  {
-    name: "The Crypt",
-    containers: [],
-    key: 6,
-  },
-  {
-    name: "Grandma's Attic",
-    containers: [
-      { name: "Box 1", key: 1, items: [] },
-      { name: "Box 2", key: 2, items: [] },
-    ],
-    key: 7,
-  },
-  {
-    name: "Fruit Cellar",
-    containers: [
-      { name: "Orange", key: 1, items: [] },
-      { name: "Apples", key: 2, items: [] },
-    ],
-    key: 8,
-  },
-  {
-    name: "Shed",
-    containers: [
-      { name: "Orange", key: 1, items: [] },
-      { name: "Apples", key: 2, items: [] },
-    ],
-    key: 9,
-  },
-];
+// const data = [
+//   {
+//     name: "Basement",
+//     containers: [
+//       {
+//         name: "Box 1",
+//         key: 1,
+//         items: [
+//           { name: "item 1", description: "item 1 description", key: 1 },
+//           { name: "item 2", description: "item 2 description", key: 2 },
+//         ],
+//       },
+//       {
+//         name: "Box 2",
+//         key: 2,
+//         items: [
+//           { name: "item 1", description: "item 1 description", key: 1 },
+//           { name: "item 2", description: "item 2 description", key: 2 },
+//         ],
+//       },
+//     ],
+//     key: 1,
+//   },
+//   {
+//     name: "Storage Room",
+//     containers: [
+//       {
+//         name: "Box 1",
+//         key: 1,
+//         items: [
+//           { name: "item 1", description: "item 1 description", key: 1 },
+//           { name: "item 2", description: "item 2 description", key: 2 },
+//         ],
+//       },
+//       {
+//         name: "Box 2",
+//         key: 2,
+//         items: [
+//           { name: "item 1", description: "item 1 description", key: 1 },
+//           { name: "item 2", description: "item 2 description", key: 2 },
+//         ],
+//       },
+//       {
+//         name: "Box 3",
+//         key: 3,
+//         items: [
+//           { name: "item 1", description: "item 1 description", key: 1 },
+//           { name: "item 2", description: "item 2 description", key: 2 },
+//         ],
+//       },
+//       {
+//         name: "Box 4",
+//         key: 4,
+//         items: [
+//           { name: "item 1", description: "item 1 description", key: 1 },
+//           { name: "item 2", description: "item 2 description", key: 2 },
+//         ],
+//       },
+//     ],
+//     key: 2,
+//   },
+//   {
+//     name: "Attic",
+//     containers: [
+//       {
+//         name: "Christmas Box",
+//         key: 1,
+//         items: [
+//           { name: "item 1", description: "item 1 description", key: 1 },
+//           { name: "item 2", description: "item 2 description", key: 2 },
+//         ],
+//       },
+//       {
+//         name: "Haloween",
+//         key: 2,
+//         items: [
+//           { name: "item 1", description: "item 1 description", key: 1 },
+//           { name: "item 2", description: "item 2 description", key: 2 },
+//         ],
+//       },
+//     ],
+//     key: 3,
+//   },
+//   {
+//     name: "Garage",
+//     containers: [
+//       {
+//         name: "Tool Box",
+//         key: 1,
+//         items: [
+//           { name: "item 1", description: "item 1 description", key: 1 },
+//           { name: "item 2", description: "item 2 description", key: 2 },
+//         ],
+//       },
+//       {
+//         name: "Car parts",
+//         key: 2,
+//         items: [
+//           { name: "item 1", description: "item 1 description", key: 1 },
+//           { name: "item 2", description: "item 2 description", key: 2 },
+//         ],
+//       },
+//     ],
+//     key: 4,
+//   },
+//   {
+//     name: "Spare Room",
+//     containers: [
+//       {
+//         name: "Decorations",
+//         key: 1,
+//         items: [
+//           { name: "item 1", description: "item 1 description", key: 1 },
+//           { name: "item 2", description: "item 2 description", key: 2 },
+//         ],
+//       },
+//       {
+//         name: "Blankets",
+//         key: 2,
+//         items: [
+//           { name: "item 1", description: "item 1 description", key: 1 },
+//           { name: "item 2", description: "item 2 description", key: 2 },
+//         ],
+//       },
+//     ],
+//     key: 5,
+//   },
+//   {
+//     name: "The Crypt",
+//     containers: [],
+//     key: 6,
+//   },
+//   {
+//     name: "Grandma's Attic",
+//     containers: [
+//       { name: "Box 1", key: 1, items: [] },
+//       { name: "Box 2", key: 2, items: [] },
+//     ],
+//     key: 7,
+//   },
+//   {
+//     name: "Fruit Cellar",
+//     containers: [
+//       { name: "Orange", key: 1, items: [] },
+//       { name: "Apples", key: 2, items: [] },
+//     ],
+//     key: 8,
+//   },
+//   {
+//     name: "Shed",
+//     containers: [
+//       { name: "Orange", key: 1, items: [] },
+//       { name: "Apples", key: 2, items: [] },
+//     ],
+//     key: 9,
+//   },
+// ];
