f90f0bea1c26a5e83a1c35a77311b4d2845de50c
eric-sims <e.sims@icloud.com>
2022-10-27 22:22:21 -0600

    QR code generated and lets you download the pages

--- src/QRService.tsx
+++ src/QRService.tsx
@@ -1,24 +1,30 @@
+import { Linking } from "react-native";
 
-
-export function printQRCodes(container_array : any[]) {
-    const containers = container_array.map((container) => {
-        return {
-            "containerID": container.id,
-            "containerName": container.cName,
-            "roomName": container.pName
-        }
-    })
-    
-    var myHeaders = new Headers();
-    myHeaders.append("Content-Type", "application/json");
+export async function printQRCodes(container_array : any[]) {
+  console.log('loading...')
+  const containers = container_array.map((container) => {
+      return {
+          "containerID": container.id,
+          "containerName": container.cName,
+          "roomName": container.pName
+      }
+  })
   
-    fetch("https://q3zo38dhlg.execute-api.us-east-1.amazonaws.com/default/generateAndSaveQRCode", {
-      method: 'POST',
-      headers: myHeaders,
-      body: JSON.stringify({containers}),
-      redirect: 'follow'
+  var myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+
+  fetch("https://q3zo38dhlg.execute-api.us-east-1.amazonaws.com/default/generateAndSaveQRCode", {
+    method: 'POST',
+    headers: myHeaders,
+    body: JSON.stringify({containers}),
+    redirect: 'follow'
+  })
+    .then(response => response.text())
+    .then(async result => {
+      const res = result.replaceAll("\"","");
+      console.log(res);
+      await Linking.openURL(res);
     })
-      .then(response => response.text())
-      .then(result => console.log(result)) // TODO: Use this result to pull up a PDF viewer
-      .catch(error => console.log('error', error));
+    .catch(error => console.log('error', error));
+        
   }
